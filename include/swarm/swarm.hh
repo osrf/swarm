741b5d34981cc58a83cf9bdb3cba6811fd37c54e
Carlos Agüero <caguero@osrfoundation.org>
2015-07-15 13:11:24 -0700

    Initial layout for the project.

---
+++ include/swarm/swarm.hh
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+/// \file swarm.hh
+/// \brief Structures and functions for the SWARN API.
+
+#ifndef __SWARM_API_SWARM_HH__
+#define __SWARM_API_SWARM_HH__
+
+namespace gazebo
+{
+  namespace swarm
+  {
+    /// \brief
+    class Swarm
+    {
+      /// \brief
+      public: Swarm();
+
+      /// \brief
+      public: virtual ~Swarm();
+    };
+  }
+}
+
+#endif
