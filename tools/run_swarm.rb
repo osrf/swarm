8bc7961802de3dcc9f98759b9b01b5b09a723aed
Nate Koenig <natekoenig@gmail.com>
2015-12-15 16:04:53 -0800

    update ruby script

--- tools/run_swarm.rb
+++ tools/run_swarm.rb
@@ -120,17 +120,40 @@ Average number of neighbors per robot & \swarmAvgNeighborsRobot\\\\
 class LogParser
   include Enumerable
 
+  #################################################  
+  # Constructor
   def initialize(_logFile)
     @logFile = _logFile
     @file = File.open(@logFile, 'rb')
     @header = nil
+    @timeStep = 0.1
+
+    @duration = 0
+    @wrongBooReports = 0
+    @succeed = false
+
+    @totalMsgsSent = 0
+    @totalMsgsUnicast = 0
+    @totalMsgsBroadcast = 0
+    @totalMsgsMulticast = 0
+    @totalMsgsSentFreq = 0
+
+    @counterMsgsSentFreq = 0
+    @totalDropRatio = 0
+    @counterDropRatio = 0
+    @totalDataRate = 0
+    @counterDataRate = 0
+    @totalNeighbors = 0
+    @counterTotalNeighbors = 0
   end
 
   #################################################  
+  # Get the next message in a log file, or nil if 
+  # the end of file is reached
   def nextMsg
     # Read the 4-byte header
     sizeString = @file.read(4)
-    if sizeString.length < 4 then
+    if sizeString.nil? || sizeString.length < 4
       @file.close
       return nil
     end
@@ -146,6 +169,11 @@ class LogParser
     if @header.nil?
       @header = Swarm::Msgs::LogHeader.new
       @header.decode(msg)
+      if @header.field?(:time_step)
+        @timeStep = @header.time_step
+      end
+
+      # Return the next actual message
       return nextMsg
     end
 
@@ -156,11 +184,241 @@ class LogParser
   end
 
   ###############################################
-  # Implement an each method
+  # Convert log file to CSV file
+  def to_csv(_file)
+    file = File.open(_file, 'w')
+    file.print "# time, msg_sent, msg_freq, num_unicast, num_broadcast, num_multicast, potential_recipients, msgs_delivered, drop_ratio, bytes_sent, data_rate, avg_num_neighbors\n"
+
+    # Process each message in the logfile
+    self.each do |entry|
+      # Check for the 'incoming_msgs' field,
+      # which tells us about what happened to
+      # messages that were sent
+      if entry.field?(:incoming_msgs) and entry.field?(:time) and
+         entry.field?(:visibility)
+        time = entry.time
+      
+        numMsgsSent = 0
+        numUnicastSent = 0
+        numBroadcastSent = 0
+        numMulticastSent = 0
+        freqMsgsSent = 0
+        numRecipients = 0
+        numMsgsDelivered = 0
+        dropRatio = 0
+        bytesSent = 0
+        dataRate = 0
+        numNeighbors = 0
+        numRobots = 0
+        avgNumNeighbors = 0
+
+        entry.incoming_msgs.message.each do |msg|
+          if msg.dst_address == 'broadcast'
+            numBroadcastSent += 1
+          elsif msg.dst_address == 'multicast'
+            numMulticastSent += 1
+          else
+            numUnicastSent += 1
+          end
+
+          numMsgsSent += 1
+          bytesSent += msg.size + 56
+          msg.neighbor.each do |neighbor|
+            numRecipients += 1
+            if neighbor.status == Swarm::Msgs::CommsStatus::DELIVERED
+              numMsgsDelivered += 1
+            end
+          end
+        end
+
+        entry.visibility.row.each do |msg|
+          if msg.src == 'boo' 
+            next
+          end
+
+          numRobots += 1
+          msg.entry.each do |neighbor|
+            if neighbor.status == 1
+              numNeighbors += 1
+            end
+          end
+        end
+
+        if numRobots > 0
+          avgNumNeighbors = numNeighbors / numRobots.to_f
+        end
+
+        freqMsgsSent = numMsgsSent / @timeStep
+        dataRate = (bytesSent * 8) / @timeStep
+
+        if numRecipients > 0
+          dropRatio = (numRecipients - numMsgsDelivered)/numRecipients.to_f
+        end
+
+        file.print "%f,%d,%f,%d,%d,%d,%d,%d,%f,%d,%f,%f\n" %
+            [time, numMsgsSent, freqMsgsSent, numUnicastSent, numBroadcastSent,
+             numMulticastSent, numRecipients, numMsgsDelivered, dropRatio,
+             bytesSent, dataRate, avgNumNeighbors]
+
+        # Update global variables.
+        @totalMsgsSent += numMsgsSent
+        @totalMsgsUnicast += numUnicastSent
+        @totalMsgsBroadcast += numBroadcastSent
+        @totalMsgsMulticast += numMulticastSent
+        @totalMsgsSentFreq += freqMsgsSent
+
+        @counterMsgsSentFreq += 1
+        @totalDropRatio += dropRatio
+        @counterDropRatio += 1
+        @totalDataRate += dataRate
+        @counterDataRate += 1
+        @totalNeighbors += avgNumNeighbors
+        @counterTotalNeighbors += 1
+      end
+
+      entry.boo_report.each do |booReport|
+        if booReport.succeed
+          @succeed = true
+        else
+          @wrongBooReports += 1
+        end
+      end
+
+      if !@succeed
+        @duration = entry.time
+      end
+    end 
+    file.close
+  end
+
+  ###############################################
+  # Output a score
+  def score(_duration, _maxDuration, _wrongReports, _maxWrongReports)
+
+    # Duration
+    a = 0.8 * (1.0 - [1.0, _duration / _maxDuration].min)
+
+    # Wrong reports.
+    b = 0.2 * (1.0 - [1.0, _wrongReports / _maxWrongReports].min)
+
+    return a + b;
+  end
+
+
+  ###############################################
+  # Output a summary report. Make sure to run to_csv first.
+  def to_summary(_file)
+    avgMsgsSentFreq = @totalMsgsSentFreq / @counterMsgsSentFreq
+    avgDropRatio = 100.0 * @totalDropRatio / @counterDropRatio
+
+    # mbps.
+    avgDataRate = 0.000001 * @totalDataRate / @counterDataRate
+    avgNeighbors = @totalNeighbors / @counterTotalNeighbors
+
+    fd = File.open(_file, "wb")
+
+    teamName = 'Unknown'
+    numGround = 'Unknown'
+    numFixed = 'Unknown'
+    numRotor = 'Unknown'
+    terrainName = 'Unknown'
+    vegetationName = 'Unknown'
+    searchArea = 'Unknown'
+
+    # Environment.
+    if @header and @header.field?(:team_name)
+      teamName = @header.team_name
+      # Escape '_' to make latex happy.
+      teamName = teamName.gsub('_', '\_')
+    end
+
+    fd.write("\\newcommand{\\swarmTeamName}{#{teamName}}\n")
+
+    if @header and @header.field?(:num_ground_vehicles)
+      numGround = @header.num_ground_vehicles
+    end
+    fd.write("\\newcommand{\\swarmNumGroundVehicles}{#{numGround}}\n")
+
+    if @header and @header.field?(:num_fixed_vehicles)
+      numFixed = @header.num_fixed_vehicles
+    end
+    fd.write("\\newcommand{\\swarmNumFixedVehicles}{#{numFixed}}\n")
+
+    if @header and @header.field?(:num_rotor_vehicles)
+      numRotor = @header.num_rotor_vehicles
+    end
+    fd.write("\\newcommand{\\swarmNumRotorVehicles}{#{numRotor}}\n")
+
+    if @header and @header.field?(:terrain_name)
+      terrainName = @header.terrain_name
+      # Escape '_' to make latex happy.
+      terrainName = terrainName.gsub('_', '\_')
+    end
+    fd.write("\\newcommand{\\swarmTerrainName}{#{terrainName}}\n")
+
+    if @header and @header.field?(:vegetation_name)
+      vegetationName = @header.vegetation_name
+      # Escape '_' to make latex happy.
+      vegetationName = vegetationName.gsub('_', '\_')
+    end
+    fd.write("\\newcommand{\\swarmVegetationName}{#{vegetationName}}\n")
+
+    if @header and @header.field?(:search_area)
+      searchArea = @header.search_area
+      # Escape '_' to make latex happy.
+      searchArea = searchArea.gsub('_', '\_')
+    end
+    fd.write("\\newcommand{\\swarmSearchArea}{#{searchArea}}\n")
+
+    # Completion.
+    totalScore = 0.0
+    maxDuration = 7200
+    maxWrongReports = 20
+    if @succeed
+      totalScore = score(@duration, maxDuration, @wrongBooReports,
+                         maxWrongReports)
+      fd.write("\\newcommand{\\swarmSucceed}{Yes}\n")
+    else
+      fd.write("\\newcommand{\\swarmSucceed}{No}\n")
+    end
+
+    fd.write("\\newcommand{\\swarmWrongBooReports}{#{@wrongBooReports}}\n")
+    fd.write("\\newcommand{\\swarmDuration}{#{@duration}}\n")
+
+    if @header.field?(:max_time_allowed)
+      maxDuration = @header.max_time_allowed
+    else
+      print "Warning: <max_time_allowed> not present in log file.\n"
+    end
+
+    if @header.field?(:max_wrong_reports)
+      maxWrongReports = @header.max_wrong_reports
+    else
+      print "Warning: <max_wrong_reports> not present in log file.\n"
+    end
+
+    fd.write("\\newcommand{\\swarmScore}{#{totalScore}}\n")
+
+    # Comms summary.
+    fd.write("\\newcommand{\\swarmNumMsgsSent}{#{@totalMsgsSent}}\n")
+    fd.write("\\newcommand{\\swarmNumUnicastSent}{#{@totalMsgsUnicast}}\n")
+    fd.write("\\newcommand{\\swarmNumBroadcastSent}{#{@totalMsgsBroadcast}}\n")
+    fd.write("\\newcommand{\\swarmNumMulticastSent}{#{@totalMsgsMulticast}}\n")
+    fd.write("\\newcommand{\\swarmFreqMsgsSent}{#{avgMsgsSentFreq}}\n")
+    fd.write("\\newcommand{\\swarmAvgMsgsDrop}{#{avgDropRatio}}\n")
+    fd.write("\\newcommand{\\swarmAvgDataRateRobot}{#{avgDataRate}}\n")
+    fd.write("\\newcommand{\\swarmAvgNeighborsRobot}{#{avgNeighbors}}\n")
+    fd.write("\\newcommand{\\swarmMaxDuration}{#{maxDuration}}\n")
+    fd.write("\\newcommand{\\swarmMaxWrongReports}{#{maxWrongReports}}\n")
+    fd.close()
+  end
+
+  ###############################################
+  # Implement an 'each' method
   def each
     while true do
       msg = nextMsg
-      if msg.nil? then
+      if msg.nil?
         break
       end
       yield msg
@@ -198,13 +456,12 @@ class Runner
 
     @dir = options[:dir]
 
-    if options.has_key?(:reports) then
+    if options.has_key?(:reports)
       generate_reports(options[:reports])
     else
       run_tests
     end
   end
-
 
   ###############################################
   # Generate all the runs based on the provided parameters
@@ -218,19 +475,19 @@ class Runner
 
       # Iterate over the search areas
       @@searchAreas.each do |searchArea|
-        if searchArea < @minSearch || searchArea > @maxSearch then
+        if searchArea < @minSearch || searchArea > @maxSearch 
           next
         end
 
         # Iterate over the densities
         @@densities.each do |density|
-          if density < @minDensity || density > @maxDensity then
+          if density < @minDensity || density > @maxDensity 
             next
           end
 
           # Iterate over the vehicle counts
           @@vehicleCounts.each do |vehicleCount|
-            if vehicleCount < @minVehicle || vehicleCount > @maxVehicle then
+            if vehicleCount < @minVehicle || vehicleCount > @maxVehicle
               next
             end
 
@@ -274,7 +531,7 @@ class Runner
     # Ask before proceeding
     print "Run tests ? (y/N) "
     ans = gets.chomp
-    if ans != 'y' && ans != 'Y' then
+    if ans != 'y' && ans != 'Y'
       return
     end
 
@@ -282,7 +539,7 @@ class Runner
     time = Time.now.strftime("%Y-%m-%dT%H:%M:%S")
 
     # Clear the log directories, if specified on the command line.
-    if @clearLogs then
+    if @clearLogs
       puts "Clearing logs"
       FileUtils.rm_rf("#{ENV['HOME']}/.swarm/log")
       FileUtils.rm_rf("#{ENV['HOME']}/.gazebo/log")
@@ -338,17 +595,22 @@ class Runner
       puts "Generating #{report}"
       swarmLog = "#{report}/swarm.log"
       swarmSummary = "#{report}/swarm_summary.tex"
-      swarmCsv = "#{report}/swarm.csv"
+      swarmCSV = "#{report}/swarm.csv"
 
-      # Step 1: Create csv and summary files
-      `swarmlog_to_csv_comms.py #{swarmLog} #{swarmSummary} > #{swarmCsv}`
+      parser = LogParser.new(swarmLog)
 
-      # Step 2: Generate images
+      # Step 1: Create csv file
+      parser.to_csv(swarmCSV)
+      
+      # Step 2: Create the summary file
+      parser.to_csv(swarmSummary)
+
+      # Step 3: Generate images
       plots.each do |plot|
-        `output_dir=#{report} logfile=#{swarmCsv} #{plot}`
+        `output_dir=#{report} logfile=#{swarmCSV} #{plot}`
       end
 
-      # Step 3: Generate final report
+      # Step 4: Generate final report
       File.open("#{report}/report.tex", 'w') {|file|
         file.write($latexTemplate)
       }
@@ -422,18 +684,13 @@ if __FILE__ == $0
       options[:reps] = n
     end
 
-    opts.on("-g DIR", "--generate-report DIR", String, "Generate reports for a directory") do |n|
+    opts.on("-g DIR", "--generate-report DIR", String,
+            "Generate reports for a directory") do |n|
       options[:reports] = n
     end
 
   end.parse!
 
-  parser = LogParser.new("/data/swarm_logs/2015-12-10T07:50:32/swarm/2015-12-10T07:50:40.110786/swarm.log")
-
-  parser.each do |msg|
-    puts msg.to_json
-  end 
-
   # Create the runner
-  # runner = Runner.new(options)
+  runner = Runner.new(options)
 end
